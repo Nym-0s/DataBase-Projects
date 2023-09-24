67b9d00e5375d709d9655424e02570814f8f0f11
Line Yedinak <lotosofhellforyou@gmail.com>
2023-09-24 13:36:38 +0300

    Added task 6 & 7 (groupboxes, textboxes, buttons, changed window size)

--- Lab02/Lab02/MainForm.Designer.cs
+++ Lab02/Lab02/MainForm.Designer.cs
@@ -50,6 +50,12 @@
             GradeTB = new TextBox();
             groupBox5 = new GroupBox();
             button5 = new Button();
+            groupBox6 = new GroupBox();
+            button6 = new Button();
+            this.classroomTBTeach = new TextBox();
+            groupBox7 = new GroupBox();
+            button7 = new Button();
+            gradeTBTeach = new TextBox();
             menuStrip1.SuspendLayout();
             statusStrip1.SuspendLayout();
             groupBox1.SuspendLayout();
@@ -57,6 +63,8 @@
             groupBox3.SuspendLayout();
             groupBox4.SuspendLayout();
             groupBox5.SuspendLayout();
+            groupBox6.SuspendLayout();
+            groupBox7.SuspendLayout();
             SuspendLayout();
             // 
             // menuStrip1
@@ -66,7 +74,7 @@
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Padding = new Padding(7, 3, 0, 3);
-            menuStrip1.Size = new Size(920, 30);
+            menuStrip1.Size = new Size(926, 30);
             menuStrip1.TabIndex = 0;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -89,40 +97,40 @@
             // 
             statusStrip1.ImageScalingSize = new Size(20, 20);
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabelNumber });
-            statusStrip1.Location = new Point(0, 550);
+            statusStrip1.Location = new Point(0, 587);
             statusStrip1.Name = "statusStrip1";
             statusStrip1.Padding = new Padding(1, 0, 16, 0);
-            statusStrip1.Size = new Size(920, 26);
+            statusStrip1.Size = new Size(926, 26);
             statusStrip1.TabIndex = 2;
             statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(80, 20);
-            toolStripStatusLabel1.Text = "Line count:";
+            toolStripStatusLabel1.Size = new Size(86, 20);
+            toolStripStatusLabel1.Text = "Time count:";
             // 
             // toolStripStatusLabelNumber
             // 
             toolStripStatusLabelNumber.Name = "toolStripStatusLabelNumber";
-            toolStripStatusLabelNumber.Size = new Size(60, 20);
-            toolStripStatusLabelNumber.Text = "number";
+            toolStripStatusLabelNumber.Size = new Size(39, 20);
+            toolStripStatusLabelNumber.Text = "time";
             // 
             // result
             // 
             result.Anchor = AnchorStyles.None;
-            result.Location = new Point(433, 27);
+            result.Location = new Point(436, 43);
             result.Margin = new Padding(3, 4, 3, 4);
             result.Multiline = true;
             result.Name = "result";
             result.ScrollBars = ScrollBars.Vertical;
-            result.Size = new Size(456, 503);
+            result.Size = new Size(456, 526);
             result.TabIndex = 3;
             // 
             // StSurname
             // 
             StSurname.Cursor = Cursors.IBeam;
-            StSurname.Location = new Point(16, 36);
+            StSurname.Location = new Point(16, 26);
             StSurname.Name = "StSurname";
             StSurname.PlaceholderText = " Student's Surname";
             StSurname.Size = new Size(222, 27);
@@ -135,7 +143,7 @@
             groupBox1.Controls.Add(StSurname);
             groupBox1.Location = new Point(12, 43);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(395, 89);
+            groupBox1.Size = new Size(395, 70);
             groupBox1.TabIndex = 6;
             groupBox1.TabStop = false;
             groupBox1.Text = "Task 1 (Get student's teachers)";
@@ -144,7 +152,7 @@
             // 
             button1.BackColor = Color.AliceBlue;
             button1.Cursor = Cursors.Hand;
-            button1.Location = new Point(266, 36);
+            button1.Location = new Point(266, 26);
             button1.Name = "button1";
             button1.Size = new Size(101, 27);
             button1.TabIndex = 6;
@@ -155,7 +163,7 @@
             // StSurname2
             // 
             StSurname2.Cursor = Cursors.IBeam;
-            StSurname2.Location = new Point(16, 36);
+            StSurname2.Location = new Point(16, 26);
             StSurname2.Name = "StSurname2";
             StSurname2.PlaceholderText = " Student's Surname";
             StSurname2.Size = new Size(222, 27);
@@ -166,9 +174,9 @@
             groupBox2.BackColor = Color.AliceBlue;
             groupBox2.Controls.Add(button2);
             groupBox2.Controls.Add(StSurname2);
-            groupBox2.Location = new Point(12, 138);
+            groupBox2.Location = new Point(12, 119);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(395, 89);
+            groupBox2.Size = new Size(395, 70);
             groupBox2.TabIndex = 7;
             groupBox2.TabStop = false;
             groupBox2.Text = "Task 2 (Find bus by student)";
@@ -177,7 +185,7 @@
             // 
             button2.BackColor = Color.Lavender;
             button2.Cursor = Cursors.Hand;
-            button2.Location = new Point(266, 36);
+            button2.Location = new Point(266, 26);
             button2.Name = "button2";
             button2.Size = new Size(101, 27);
             button2.TabIndex = 7;
@@ -188,7 +196,7 @@
             // TSurname
             // 
             TSurname.Cursor = Cursors.IBeam;
-            TSurname.Location = new Point(16, 36);
+            TSurname.Location = new Point(16, 26);
             TSurname.Name = "TSurname";
             TSurname.PlaceholderText = " Teacher's Surname";
             TSurname.Size = new Size(222, 27);
@@ -199,9 +207,9 @@
             groupBox3.BackColor = Color.Lavender;
             groupBox3.Controls.Add(button3);
             groupBox3.Controls.Add(TSurname);
-            groupBox3.Location = new Point(12, 233);
+            groupBox3.Location = new Point(12, 195);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(395, 89);
+            groupBox3.Size = new Size(395, 70);
             groupBox3.TabIndex = 7;
             groupBox3.TabStop = false;
             groupBox3.Text = "Task 3 (Get teachers student list)";
@@ -210,7 +218,7 @@
             // 
             button3.BackColor = Color.AliceBlue;
             button3.Cursor = Cursors.Hand;
-            button3.Location = new Point(266, 36);
+            button3.Location = new Point(266, 26);
             button3.Name = "button3";
             button3.Size = new Size(101, 27);
             button3.TabIndex = 8;
@@ -221,7 +229,7 @@
             // BusNumber
             // 
             BusNumber.Cursor = Cursors.IBeam;
-            BusNumber.Location = new Point(16, 36);
+            BusNumber.Location = new Point(16, 26);
             BusNumber.Name = "BusNumber";
             BusNumber.PlaceholderText = " Bus number";
             BusNumber.Size = new Size(222, 27);
@@ -232,9 +240,9 @@
             groupBox4.BackColor = Color.AliceBlue;
             groupBox4.Controls.Add(button4);
             groupBox4.Controls.Add(BusNumber);
-            groupBox4.Location = new Point(12, 328);
+            groupBox4.Location = new Point(12, 271);
             groupBox4.Name = "groupBox4";
-            groupBox4.Size = new Size(395, 89);
+            groupBox4.Size = new Size(395, 70);
             groupBox4.TabIndex = 8;
             groupBox4.TabStop = false;
             groupBox4.Text = "Task 4 (Find students by their bus)";
@@ -243,7 +251,7 @@
             // 
             button4.BackColor = Color.Lavender;
             button4.Cursor = Cursors.Hand;
-            button4.Location = new Point(266, 36);
+            button4.Location = new Point(266, 26);
             button4.Name = "button4";
             button4.Size = new Size(101, 27);
             button4.TabIndex = 10;
@@ -254,7 +262,7 @@
             // GradeTB
             // 
             GradeTB.Cursor = Cursors.IBeam;
-            GradeTB.Location = new Point(16, 36);
+            GradeTB.Location = new Point(16, 26);
             GradeTB.Name = "GradeTB";
             GradeTB.PlaceholderText = " Grade";
             GradeTB.Size = new Size(222, 27);
@@ -265,9 +273,9 @@
             groupBox5.BackColor = Color.Lavender;
             groupBox5.Controls.Add(button5);
             groupBox5.Controls.Add(GradeTB);
-            groupBox5.Location = new Point(12, 423);
+            groupBox5.Location = new Point(12, 347);
             groupBox5.Name = "groupBox5";
-            groupBox5.Size = new Size(395, 89);
+            groupBox5.Size = new Size(395, 70);
             groupBox5.TabIndex = 7;
             groupBox5.TabStop = false;
             groupBox5.Text = "Task 5 (Find students by grade)";
@@ -276,7 +284,7 @@
             // 
             button5.BackColor = Color.AliceBlue;
             button5.Cursor = Cursors.Hand;
-            button5.Location = new Point(266, 36);
+            button5.Location = new Point(266, 26);
             button5.Name = "button5";
             button5.Size = new Size(101, 27);
             button5.TabIndex = 12;
@@ -284,11 +292,77 @@
             button5.UseVisualStyleBackColor = false;
             button5.Click += button5_Click;
             // 
+            // groupBox6
+            // 
+            groupBox6.BackColor = Color.AliceBlue;
+            groupBox6.Controls.Add(button6);
+            groupBox6.Controls.Add(this.classroomTBTeach);
+            groupBox6.Location = new Point(12, 423);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(395, 70);
+            groupBox6.TabIndex = 13;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "Task 6 (Find teachers by classroom)";
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Lavender;
+            button6.Cursor = Cursors.Hand;
+            button6.Location = new Point(266, 26);
+            button6.Name = "button6";
+            button6.Size = new Size(101, 27);
+            button6.TabIndex = 12;
+            button6.Text = "Find";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // classroomTBTeach
+            // 
+            this.classroomTBTeach.Cursor = Cursors.IBeam;
+            this.classroomTBTeach.Location = new Point(16, 26);
+            this.classroomTBTeach.Name = "classroomTBTeach";
+            this.classroomTBTeach.PlaceholderText = " Classroom";
+            this.classroomTBTeach.Size = new Size(222, 27);
+            this.classroomTBTeach.TabIndex = 5;
+            // 
+            // groupBox7
+            // 
+            groupBox7.BackColor = Color.Lavender;
+            groupBox7.Controls.Add(button7);
+            groupBox7.Controls.Add(gradeTBTeach);
+            groupBox7.Location = new Point(12, 499);
+            groupBox7.Name = "groupBox7";
+            groupBox7.Size = new Size(395, 70);
+            groupBox7.TabIndex = 14;
+            groupBox7.TabStop = false;
+            groupBox7.Text = "Task 7 (Find teachers by grade)";
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.AliceBlue;
+            button7.Cursor = Cursors.Hand;
+            button7.Location = new Point(266, 26);
+            button7.Name = "button7";
+            button7.Size = new Size(101, 27);
+            button7.TabIndex = 12;
+            button7.Text = "Find";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // gradeTBTeach
+            // 
+            gradeTBTeach.Cursor = Cursors.IBeam;
+            gradeTBTeach.Location = new Point(16, 26);
+            gradeTBTeach.Name = "gradeTBTeach";
+            gradeTBTeach.PlaceholderText = " Grade";
+            gradeTBTeach.Size = new Size(222, 27);
+            gradeTBTeach.TabIndex = 5;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(920, 576);
+            ClientSize = new Size(926, 613);
+            Controls.Add(groupBox7);
+            Controls.Add(groupBox6);
             Controls.Add(groupBox5);
             Controls.Add(groupBox4);
             Controls.Add(groupBox3);
@@ -315,6 +389,10 @@
             groupBox4.PerformLayout();
             groupBox5.ResumeLayout(false);
             groupBox5.PerformLayout();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
+            groupBox7.ResumeLayout(false);
+            groupBox7.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -346,6 +424,12 @@
         private Button button6;
         private Button button7;
         private Button button8;
+        private GroupBox groupBox6;
+        private TextBox textBox1;
+        private GroupBox groupBox7;
+        private TextBox gradeTBTeach;
+        private TextBox textBox3;
+        private GroupBox groupBox8;
     }
 
 }
