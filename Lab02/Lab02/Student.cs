d6e68cd2356e353b857c561c0cbef898c56de56c
Line Yedinak <lotosofhellforyou@gmail.com>
2023-09-24 10:47:15 +0300

    Changed Student.cs

--- Lab02/Lab02/Student.cs
+++ Lab02/Lab02/Student.cs
@@ -1,4 +1,10 @@
-﻿namespace Lab02
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
 {
     internal class Student
     {
